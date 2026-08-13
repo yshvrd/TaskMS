fa43b2c297d6fce6d340311705c2a2b37351759f
yshvrd <yshvrd@gmail.com>
2026-08-14 02:06:13 +0530

    Add CORSMiddleware

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
 from routes.auth import router as auth_router
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI(title="TaskMS")
 
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth_router)
 
